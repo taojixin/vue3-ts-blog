ffe5100436e4da12c073e6a4ac21a43013e7630f
Tao <491675919@qq.com>
2022-08-11 17:05:34 +0800

    axios封装

--- vue3-blog/src/main.ts
+++ vue3-blog/src/main.ts
@@ -6,7 +6,21 @@ import ynRequest from './service'
 
 createApp(App).use(store).use(router).mount('#app')
 
+// ynRequest.request({
+//   url: '/api/comment/getcomments',
+//   method: 'GET'
+// })
 ynRequest.request({
   url: '/api/comment/getcomments',
-  method: 'GET'
+  method: 'GET',
+  interceptors: {
+    requestInterceptor: (config) => {
+      console.log('单独请求的config')
+      return config
+    },
+    responseInterceptor: (res) => {
+      console.log('单独响应的拦截器')
+      return res
+    }
+  }
 })
--- vue3-blog/src/service/request/index.ts
+++ vue3-blog/src/service/request/index.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import type { AxiosInstance, AxiosRequestConfig } from 'axios'
+import type { AxiosInstance } from 'axios'
 import { YNRequestConfig, YNRequestInterceptors } from './type'
 
 class YNRequest {
@@ -9,6 +9,8 @@ class YNRequest {
   constructor(config: YNRequestConfig) {
     this.instance = axios.create(config)
     this.insterceptors = config.interceptors
+
+    // 单个实例的拦截器
     this.instance.interceptors.request.use(
       this.insterceptors?.requestInterceptor,
       this.insterceptors?.requestInterceptorCatch
@@ -17,10 +19,39 @@ class YNRequest {
       this.insterceptors?.responseInterceptor,
       this.insterceptors?.requestInterceptorCatch
     )
+
+    // 所有实例都有的拦截器
+    this.instance.interceptors.request.use(
+      (config) => {
+        console.log('所有实例都有的拦截器：请求拦截成功')
+        return config
+      },
+      (err) => {
+        console.log('所有实例都有的拦截器：请求拦截失败')
+        return err
+      }
+    )
+    this.instance.interceptors.response.use(
+      (config) => {
+        console.log('所有实例都有的拦截器：响应拦截成功')
+        return config
+      },
+      (err) => {
+        console.log('所有实例都有的拦截器：响应拦截成功')
+        return err
+      }
+    )
   }
 
-  request(config: AxiosRequestConfig): void {
+  request(config: YNRequestConfig): void {
+    // 给某个单独的请求添加拦截器
+    if (config.interceptors?.requestInterceptor) {
+      config = config.interceptors.requestInterceptor(config)
+    }
     this.instance.request(config).then((res) => {
+      if (config.interceptors?.responseInterceptor) {
+        res = config.interceptors.responseInterceptor(res)
+      }
       console.log(res)
     })
   }
